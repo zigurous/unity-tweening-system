259da10d849520fd22c30b2056474af1ada364b2
Adam Graham <adam@zigurous.com>
2021-02-08 01:22:45 -0600

    Add static class to store general tweening settings

---
+++ Runtime/Settings.cs
@@ -0,0 +1,16 @@
+﻿namespace Zigurous.Animation.Tweening
+{
+    public static class Settings
+    {
+        public static Ease defaultEase = Ease.QuadOut;
+        public static bool defaultReversed = false;
+        public static bool defaultAutoStart = true;
+        public static bool defaultAutoKill = true;
+        public static bool defaultRecyclable = true;
+        public static float defaultDuration = 0.3f;
+        public static float defaultDelay = 0.0f;
+        public static float overshoot = 1.70158f;
+
+    }
+
+}
