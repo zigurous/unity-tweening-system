8db394a2259c7b9108e8b3a0ae589fc4e805543c
Adam Graham <adam@zigurous.com>
2021-02-08 01:21:46 -0600

    Add Ease definitions

---
+++ Runtime/Ease.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Zigurous.Animation.Tweening
+{
+    public enum Ease : int
+    {
+        Linear,
+        SineIn,
+        SineOut,
+        SineInOut,
+        CubicIn,
+        CubicOut,
+        CubicInOut,
+        QuadIn,
+        QuadOut,
+        QuadInOut,
+        QuartIn,
+        QuartOut,
+        QuartInOut,
+        QuintIn,
+        QuintOut,
+        QuintInOut,
+        ExpoIn,
+        ExpoOut,
+        ExpoInOut,
+        CircIn,
+        CircOut,
+        CircInOut,
+        BackIn,
+        BackOut,
+        BackInOut,
+        ElasticIn,
+        ElasticOut,
+        ElasticInOut,
+        BounceIn,
+        BounceOut,
+        BounceInOut
+    }
+
+    public sealed class EaseEqualityComparer : IEqualityComparer<Ease>
+    {
+        public bool Equals(Ease x, Ease y) => x == y;
+
+        public int GetHashCode(Ease ease) => (int)ease;
+    }
+
+}
