ebb609c43a5bf0b830ab4756b96315c2ea486b8c
Adam Graham <adam@zigurous.com>
2021-05-21 09:29:11 -0500

    Add new CharacterJoint tweens

---
+++ Runtime/Extensions/CharacterJointTweens.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class CharacterJointTweens
+    {
+        public static Tween TweenSwingAxis(this CharacterJoint joint, Vector3 to, float duration) =>
+            Tweener.To(getter: () => joint.swingAxis,
+                       setter: swingAxis => joint.swingAxis = swingAxis,
+                       to, duration).SetTarget(joint);
+
+        public static Tween TweenProjectionDistance(this CharacterJoint joint, float to, float duration) =>
+            Tweener.To(getter: () => joint.projectionDistance,
+                       setter: projectionDistance => joint.projectionDistance = projectionDistance,
+                       to, duration).SetTarget(joint);
+
+        public static Tween TweenProjectionAngle(this CharacterJoint joint, float to, float duration) =>
+            Tweener.To(getter: () => joint.projectionAngle,
+                       setter: projectionAngle => joint.projectionAngle = projectionAngle,
+                       to, duration).SetTarget(joint);
+
+    }
+
+}
