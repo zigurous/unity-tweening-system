0bee65d1559ed3991b62a25d45f6ebcaa77cfbe1
Adam Graham <adam@zigurous.com>
2021-05-21 09:29:31 -0500

    Add new ConstantForce tweens

---
+++ Runtime/Extensions/ConstantForceTweens.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class ConstantForceTweens
+    {
+        public static Tween TweenForce(this ConstantForce constantForce, Vector3 to, float duration) =>
+            Tweener.To(getter: () => constantForce.force,
+                       setter: force => constantForce.force = force,
+                       to, duration).SetTarget(constantForce);
+
+        public static Tween TweenRelativeForce(this ConstantForce constantForce, Vector3 to, float duration) =>
+            Tweener.To(getter: () => constantForce.relativeForce,
+                       setter: relativeForce => constantForce.relativeForce = relativeForce,
+                       to, duration).SetTarget(constantForce);
+
+        public static Tween TweenTorque(this ConstantForce constantForce, Vector3 to, float duration) =>
+            Tweener.To(getter: () => constantForce.torque,
+                       setter: torque => constantForce.torque = torque,
+                       to, duration).SetTarget(constantForce);
+
+        public static Tween TweenRelativeTorque(this ConstantForce constantForce, Vector3 to, float duration) =>
+            Tweener.To(getter: () => constantForce.relativeTorque,
+                       setter: relativeTorque => constantForce.relativeTorque = relativeTorque,
+                       to, duration).SetTarget(constantForce);
+
+    }
+
+}
