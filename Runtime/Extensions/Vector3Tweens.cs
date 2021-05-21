4bbfbc010d14afd08d7eee3b05dfc45eab0d66ae
Adam Graham <adam@zigurous.com>
2021-05-21 09:32:17 -0500

    Add new Vector3 tweens

---
+++ Runtime/Extensions/Vector3Tweens.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class Vector3Tweens
+    {
+        public static Tween TweenX(this Vector3 vector, float to, float duration) =>
+            Tweener.To(getter: () => vector.x,
+                       setter: x => vector.x = x,
+                       to, duration).SetId(vector.GetHashCode());
+
+        public static Tween TweenY(this Vector3 vector, float to, float duration) =>
+            Tweener.To(getter: () => vector.y,
+                       setter: y => vector.y = y,
+                       to, duration).SetId(vector.GetHashCode());
+
+        public static Tween TweenZ(this Vector3 vector, float to, float duration) =>
+            Tweener.To(getter: () => vector.z,
+                       setter: z => vector.z = z,
+                       to, duration).SetId(vector.GetHashCode());
+
+    }
+
+}
