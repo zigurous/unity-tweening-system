007a45d5a6dcfec55c4c966b7c9c6d612dea4bba
Adam Graham <adam@zigurous.com>
2021-05-21 09:29:19 -0500

    Add new Color tweens

---
+++ Runtime/Extensions/ColorTweens.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class ColorTweens
+    {
+        public static Tween TweenRed(this Color color, float to, float duration) =>
+            Tweener.To(getter: () => color.r,
+                       setter: red => color.r = red,
+                       to, duration).SetId(color.GetHashCode());
+
+        public static Tween TweenGreen(this Color color, float to, float duration) =>
+            Tweener.To(getter: () => color.g,
+                       setter: green => color.g = green,
+                       to, duration).SetId(color.GetHashCode());
+
+        public static Tween TweenBlue(this Color color, float to, float duration) =>
+            Tweener.To(getter: () => color.b,
+                       setter: blue => color.b = blue,
+                       to, duration).SetId(color.GetHashCode());
+
+        public static Tween TweenAlpha(this Color color, float to, float duration) =>
+            Tweener.To(getter: () => color.a,
+                       setter: alpha => color.a = alpha,
+                       to, duration).SetId(color.GetHashCode());
+
+    }
+
+}
