34a61ba7f34ad462ed3c25d92c32b5816cfbb030
Adam Graham <adam@zigurous.com>
2021-05-21 09:28:30 -0500

    Add new BoxCollider tweens

---
+++ Runtime/Extensions/BoxColliderTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class BoxColliderTweens
+    {
+        public static Tween TweenCenter(this BoxCollider collider, Vector3 to, float duration) =>
+            Tweener.To(getter: () => collider.center,
+                       setter: center => collider.center = center,
+                       to, duration).SetTarget(collider);
+
+        public static Tween TweenSize(this BoxCollider collider, Vector3 to, float duration) =>
+            Tweener.To(getter: () => collider.size,
+                       setter: size => collider.size = size,
+                       to, duration).SetTarget(collider);
+
+    }
+
+}
