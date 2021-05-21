95d21667d92b3a95974442dce81505bf0c6a19eb
Adam Graham <adam@zigurous.com>
2021-05-21 09:27:57 -0500

    Add new AudioSource tweens

--- Runtime/Extensions/AudioTweens.cs
+++ Runtime/Extensions/AudioTweens.cs
@@ -4,6 +4,11 @@ namespace Zigurous.Animation.Tweening
 {
     public static class AudioTweens
     {
+        public static Tween TweenTime(this AudioSource audio, float to, float duration) =>
+            Tweener.To(getter: () => audio.time,
+                       setter: time => audio.time = time,
+                       to, duration).SetTarget(audio);
+
         public static Tween TweenPitch(this AudioSource audio, float to, float duration) =>
             Tweener.To(getter: () => audio.pitch,
                        setter: pitch => audio.pitch = pitch,
@@ -12,6 +17,41 @@ namespace Zigurous.Animation.Tweening
         public static Tween TweenVolume(this AudioSource audio, float to, float duration) =>
             Tweener.To(getter: () => audio.volume,
                        setter: volume => audio.volume = volume,
+                       to, duration).SetTarget(audio);
+
+        public static Tween TweenPanStereo(this AudioSource audio, float to, float duration) =>
+            Tweener.To(getter: () => audio.panStereo,
+                       setter: panStereo => audio.panStereo = panStereo,
+                       to, duration).SetTarget(audio);
+
+        public static Tween TweenSpatialBlend(this AudioSource audio, float to, float duration) =>
+            Tweener.To(getter: () => audio.spatialBlend,
+                       setter: spatialBlend => audio.spatialBlend = spatialBlend,
+                       to, duration).SetTarget(audio);
+
+        public static Tween TweenReverbZoneMix(this AudioSource audio, float to, float duration) =>
+            Tweener.To(getter: () => audio.reverbZoneMix,
+                       setter: reverbZoneMix => audio.reverbZoneMix = reverbZoneMix,
+                       to, duration).SetTarget(audio);
+
+        public static Tween TweenDopplerLevel(this AudioSource audio, float to, float duration) =>
+            Tweener.To(getter: () => audio.dopplerLevel,
+                       setter: dopplerLevel => audio.dopplerLevel = dopplerLevel,
+                       to, duration).SetTarget(audio);
+
+        public static Tween TweenSpread(this AudioSource audio, float to, float duration) =>
+            Tweener.To(getter: () => audio.spread,
+                       setter: spread => audio.spread = spread,
+                       to, duration).SetTarget(audio);
+
+        public static Tween TweenMinDistance(this AudioSource audio, float to, float duration) =>
+            Tweener.To(getter: () => audio.minDistance,
+                       setter: minDistance => audio.minDistance = minDistance,
+                       to, duration).SetTarget(audio);
+
+        public static Tween TweenMaxDistance(this AudioSource audio, float to, float duration) =>
+            Tweener.To(getter: () => audio.maxDistance,
+                       setter: maxDistance => audio.maxDistance = maxDistance,
                        to, duration).SetTarget(audio);
 
     }
