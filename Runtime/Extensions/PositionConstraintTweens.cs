f849158ba29a6530b23e6f56357277896a24c785
Adam Graham <adam@zigurous.com>
2021-05-23 14:30:42 -0500

    Add new PositionConstraint tweens

---
+++ Runtime/Extensions/PositionConstraintTweens.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+
+namespace Zigurous.TweenEngine
+{
+    public static class PositionConstraintTweens
+    {
+        public static Tween TweenWeight(this PositionConstraint constraint, float to, float duration) =>
+            Tweening.To(getter: () => constraint.weight,
+                        setter: weight => constraint.weight = weight,
+                        to, duration).SetTarget(constraint);
+
+        public static Tween TweenTranslationAtRest(this PositionConstraint constraint, Vector3 to, float duration) =>
+            Tweening.To(getter: () => constraint.translationAtRest,
+                        setter: translationAtRest => constraint.translationAtRest = translationAtRest,
+                        to, duration).SetTarget(constraint);
+
+        public static Tween TweenTranslationOffset(this PositionConstraint constraint, Vector3 to, float duration) =>
+            Tweening.To(getter: () => constraint.translationOffset,
+                        setter: translationOffset => constraint.translationOffset = translationOffset,
+                        to, duration).SetTarget(constraint);
+
+    }
+
+}
