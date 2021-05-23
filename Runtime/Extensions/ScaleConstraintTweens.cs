41eb58d80e0f60b359695b91a0340f04795e956e
Adam Graham <adam@zigurous.com>
2021-05-23 14:33:23 -0500

    Add new ScaleConstraint tweens

---
+++ Runtime/Extensions/ScaleConstraintTweens.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+
+namespace Zigurous.TweenEngine
+{
+    public static class ScaleConstraintTweens
+    {
+        public static Tween TweenWeight(this ScaleConstraint constraint, float to, float duration) =>
+            Tweening.To(getter: () => constraint.weight,
+                        setter: weight => constraint.weight = weight,
+                        to, duration).SetTarget(constraint);
+
+        public static Tween TweenScaleAtRest(this ScaleConstraint constraint, Vector3 to, float duration) =>
+            Tweening.To(getter: () => constraint.scaleAtRest,
+                        setter: scaleAtRest => constraint.scaleAtRest = scaleAtRest,
+                        to, duration).SetTarget(constraint);
+
+        public static Tween TweenScaleOffset(this ScaleConstraint constraint, Vector3 to, float duration) =>
+            Tweening.To(getter: () => constraint.scaleOffset,
+                        setter: scaleOffset => constraint.scaleOffset = scaleOffset,
+                        to, duration).SetTarget(constraint);
+
+    }
+
+}
