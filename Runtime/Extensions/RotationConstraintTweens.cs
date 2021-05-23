192709d1ffba4e9dbbf7c98fc12554f0262d4325
Adam Graham <adam@zigurous.com>
2021-05-23 14:33:13 -0500

    Add new RotationConstraint tweens

---
+++ Runtime/Extensions/RotationConstraintTweens.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+
+namespace Zigurous.TweenEngine
+{
+    public static class RotationConstraintTweens
+    {
+        public static Tween TweenWeight(this RotationConstraint constraint, float to, float duration) =>
+            Tweening.To(getter: () => constraint.weight,
+                        setter: weight => constraint.weight = weight,
+                        to, duration).SetTarget(constraint);
+
+        public static Tween TweenRotationAtRest(this RotationConstraint constraint, Vector3 to, float duration) =>
+            Tweening.To(getter: () => constraint.rotationAtRest,
+                        setter: rotationAtRest => constraint.rotationAtRest = rotationAtRest,
+                        to, duration).SetTarget(constraint);
+
+        public static Tween TweenRotationOffset(this RotationConstraint constraint, Vector3 to, float duration) =>
+            Tweening.To(getter: () => constraint.rotationOffset,
+                        setter: rotationOffset => constraint.rotationOffset = rotationOffset,
+                        to, duration).SetTarget(constraint);
+
+    }
+
+}
