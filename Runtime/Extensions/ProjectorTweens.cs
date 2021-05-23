fe8360ddc5ae1571c261cb30cfd1aa79a7121796
Adam Graham <adam@zigurous.com>
2021-05-23 06:57:13 -0500

    Add new Projector tweens

---
+++ Runtime/Extensions/ProjectorTweens.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class ProjectorTweens
+    {
+        public static Tween TweenNearClipPlane(this Projector projector, float to, float duration) =>
+            Tweener.To(getter: () => projector.nearClipPlane,
+                       setter: nearClipPlane => projector.nearClipPlane = nearClipPlane,
+                       to, duration).SetTarget(projector);
+
+        public static Tween TweenFarClipPlane(this Projector projector, float to, float duration) =>
+            Tweener.To(getter: () => projector.farClipPlane,
+                       setter: farClipPlane => projector.farClipPlane = farClipPlane,
+                       to, duration).SetTarget(projector);
+
+        public static Tween TweenFieldOfView(this Projector projector, float to, float duration) =>
+            Tweener.To(getter: () => projector.fieldOfView,
+                       setter: fieldOfView => projector.fieldOfView = fieldOfView,
+                       to, duration).SetTarget(projector);
+
+        public static Tween TweenAspectRatio(this Projector projector, float to, float duration) =>
+            Tweener.To(getter: () => projector.aspectRatio,
+                       setter: aspectRatio => projector.aspectRatio = aspectRatio,
+                       to, duration).SetTarget(projector);
+
+        public static Tween TweenOrthographicSize(this Projector projector, float to, float duration) =>
+            Tweener.To(getter: () => projector.orthographicSize,
+                       setter: orthographicSize => projector.orthographicSize = orthographicSize,
+                       to, duration).SetTarget(projector);
+
+    }
+
+}
