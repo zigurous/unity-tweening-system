3dc2962e7f97d3e7133c07f8d04a2583795a5d0e
Adam Graham <adam@zigurous.com>
2021-05-23 07:28:28 -0500

    Add new AudioLowPassFilter tweens

---
+++ Runtime/Extensions/AudioLowPassFilterTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class AudioLowPassFilterTweens
+    {
+        public static Tween TweenCutoffFrequency(this AudioLowPassFilter filter, float to, float duration) =>
+            Tweener.To(getter: () => filter.cutoffFrequency,
+                       setter: cutoffFrequency => filter.cutoffFrequency = cutoffFrequency,
+                       to, duration).SetTarget(filter);
+
+        public static Tween TweenLowpassResonanceQ(this AudioLowPassFilter filter, float to, float duration) =>
+            Tweener.To(getter: () => filter.lowpassResonanceQ,
+                       setter: lowpassResonanceQ => filter.lowpassResonanceQ = lowpassResonanceQ,
+                       to, duration).SetTarget(filter);
+
+    }
+
+}
