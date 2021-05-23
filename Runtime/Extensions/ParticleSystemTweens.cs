074f428cff71b17da2f055ff3f2bd6c082dbd112
Adam Graham <adam@zigurous.com>
2021-05-23 17:07:01 -0500

    Add new ParticleSystem tweens

---
+++ Runtime/Extensions/ParticleSystemTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class ParticleSystemTweens
+    {
+        public static Tween TweenTime(this ParticleSystem particleSystem, float to, float duration) =>
+            Tweening.To(getter: () => particleSystem.time,
+                        setter: time => particleSystem.time = time,
+                        to, duration).SetTarget(particleSystem);
+
+    }
+
+}
