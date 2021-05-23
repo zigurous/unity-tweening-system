f854ec16ec67899f2c9d2d898d3c503f5131c7a2
Adam Graham <adam@zigurous.com>
2021-05-23 13:57:40 -0500

    Add new HorizontalOrVerticalLayoutGroup tweens

---
+++ Runtime/Extensions/HorizontalOrVerticalLayoutGroupTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+
+namespace Zigurous.TweenEngine
+{
+    public static class HorizontalOrVerticalLayoutGroupTweens
+    {
+        public static Tween TweenSpacing(this HorizontalOrVerticalLayoutGroup group, float to, float duration) =>
+            Tweening.To(getter: () => group.spacing,
+                        setter: spacing => group.spacing = spacing,
+                        to, duration).SetTarget(group);
+
+    }
+
+}
