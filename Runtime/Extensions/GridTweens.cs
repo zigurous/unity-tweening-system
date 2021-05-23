ebbc5505d5b7a572cfa0d1fbb268448ae7f14cd8
Adam Graham <adam@zigurous.com>
2021-05-23 07:34:04 -0500

    Add new Grid tweens

---
+++ Runtime/Extensions/GridTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class GridTweens
+    {
+        public static Tween TweenCellSize(this Grid grid, Vector3 to, float duration) =>
+            Tweener.To(getter: () => grid.cellSize,
+                       setter: cellSize => grid.cellSize = cellSize,
+                       to, duration).SetTarget(grid);
+
+        public static Tween TweenCellGap(this Grid grid, Vector3 to, float duration) =>
+            Tweener.To(getter: () => grid.cellGap,
+                       setter: cellGap => grid.cellGap = cellGap,
+                       to, duration).SetTarget(grid);
+
+    }
+
+}
