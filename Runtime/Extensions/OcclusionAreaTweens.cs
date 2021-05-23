a5f834d42707704759b6d2bb6c11fead2316b184
Adam Graham <adam@zigurous.com>
2021-05-23 14:27:03 -0500

    Add new OcclusionArea tweens

---
+++ Runtime/Extensions/OcclusionAreaTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class OcclusionAreaTweens
+    {
+        public static Tween TweenCenter(this OcclusionArea area, Vector3 to, float duration) =>
+            Tweening.To(getter: () => area.center,
+                        setter: center => area.center = center,
+                        to, duration).SetTarget(area);
+
+        public static Tween TweenSize(this OcclusionArea area, Vector3 to, float duration) =>
+            Tweening.To(getter: () => area.size,
+                        setter: size => area.size = size,
+                        to, duration).SetTarget(area);
+
+    }
+
+}
