bf14aaa784af004f3ff6c88936c86bc53c671591
Adam Graham <adam@zigurous.com>
2021-05-23 13:55:33 -0500

    Add new GridLayoutGroup tweens

---
+++ Runtime/Extensions/GridLayoutGroupTweens.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zigurous.TweenEngine
+{
+    public static class GridLayoutGroupTweens
+    {
+        public static Tween TweenSpacing(this GridLayoutGroup group, Vector2 to, float duration) =>
+            Tweening.To(getter: () => group.spacing,
+                        setter: spacing => group.spacing = spacing,
+                        to, duration).SetTarget(group);
+
+        public static Tween TweenCellSize(this GridLayoutGroup group, Vector2 to, float duration) =>
+            Tweening.To(getter: () => group.cellSize,
+                        setter: cellSize => group.cellSize = cellSize,
+                        to, duration).SetTarget(group);
+
+    }
+
+}
