f5b1f02e996b9d391f5fa78af88d61e78ee1ddb9
Adam Graham <adam@zigurous.com>
2021-05-23 07:27:50 -0500

    Add new AudioEchoFilter tweens

---
+++ Runtime/Extensions/AudioEchoFilterTweens.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class AudioEchoFilterTweens
+    {
+        public static Tween TweenDelay(this AudioEchoFilter filter, float to, float duration) =>
+            Tweener.To(getter: () => filter.delay,
+                       setter: delay => filter.delay = delay,
+                       to, duration).SetTarget(filter);
+
+        public static Tween TweenDecayRatio(this AudioEchoFilter filter, float to, float duration) =>
+            Tweener.To(getter: () => filter.decayRatio,
+                       setter: decayRatio => filter.decayRatio = decayRatio,
+                       to, duration).SetTarget(filter);
+
+        public static Tween TweenDryMix(this AudioEchoFilter filter, float to, float duration) =>
+            Tweener.To(getter: () => filter.dryMix,
+                       setter: dryMix => filter.dryMix = dryMix,
+                       to, duration).SetTarget(filter);
+
+        public static Tween TweenWetMix(this AudioEchoFilter filter, float to, float duration) =>
+            Tweener.To(getter: () => filter.wetMix,
+                       setter: wetMix => filter.wetMix = wetMix,
+                       to, duration).SetTarget(filter);
+
+    }
+
+}
