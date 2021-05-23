4f81d9e12d600d6e69fcf153e7ee19e9afe9c893
Adam Graham <adam@zigurous.com>
2021-05-23 14:37:10 -0500

    Add new SpriteShapeRenderer tweens

--- Runtime/Extensions/SpriteRendererTweens.cs
+++ Runtime/Extensions/SpriteRendererTweens.cs
@@ -4,25 +4,25 @@ namespace Zigurous.TweenEngine
 {
     public static class SpriteRendererTweens
     {
-        public static Tween TweenColor(this SpriteRenderer spriteRenderer, Color to, float duration) =>
-            Tweening.To(getter: () => spriteRenderer.color,
-                        setter: color => spriteRenderer.color = color,
-                        to, duration).SetTarget(spriteRenderer);
+        public static Tween TweenColor(this SpriteRenderer renderer, Color to, float duration) =>
+            Tweening.To(getter: () => renderer.color,
+                        setter: color => renderer.color = color,
+                        to, duration).SetTarget(renderer);
 
-        public static Tween TweenAlpha(this SpriteRenderer spriteRenderer, float to, float duration) =>
-            Tweening.To(getter: () => spriteRenderer.color.a,
-                        setter: alpha => spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, alpha),
-                        to, duration).SetTarget(spriteRenderer);
+        public static Tween TweenAlpha(this SpriteRenderer renderer, float to, float duration) =>
+            Tweening.To(getter: () => renderer.color.a,
+                        setter: alpha => renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, alpha),
+                        to, duration).SetTarget(renderer);
 
-        public static Tween TweenSize(this SpriteRenderer spriteRenderer, Vector2 to, float duration) =>
-            Tweening.To(getter: () => spriteRenderer.size,
-                        setter: size => spriteRenderer.size = size,
-                        to, duration).SetTarget(spriteRenderer);
+        public static Tween TweenSize(this SpriteRenderer renderer, Vector2 to, float duration) =>
+            Tweening.To(getter: () => renderer.size,
+                        setter: size => renderer.size = size,
+                        to, duration).SetTarget(renderer);
 
-        public static Tween TweenAdaptiveModeThreshold(this SpriteRenderer spriteRenderer, float to, float duration) =>
-            Tweening.To(getter: () => spriteRenderer.adaptiveModeThreshold,
-                        setter: adaptiveModeThreshold => spriteRenderer.adaptiveModeThreshold = adaptiveModeThreshold,
-                        to, duration).SetTarget(spriteRenderer);
+        public static Tween TweenAdaptiveModeThreshold(this SpriteRenderer renderer, float to, float duration) =>
+            Tweening.To(getter: () => renderer.adaptiveModeThreshold,
+                        setter: adaptiveModeThreshold => renderer.adaptiveModeThreshold = adaptiveModeThreshold,
+                        to, duration).SetTarget(renderer);
 
     }
 
---
+++ Runtime/Extensions/SpriteShapeRendererTweens.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+
+namespace Zigurous.TweenEngine
+{
+    public static class SpriteShapeRendererTweens
+    {
+        public static Tween TweenColor(this SpriteShapeRenderer renderer, Color to, float duration) =>
+            Tweening.To(getter: () => renderer.color,
+                        setter: color => renderer.color = color,
+                        to, duration).SetTarget(renderer);
+
+        public static Tween TweenAlpha(this SpriteShapeRenderer renderer, float to, float duration) =>
+            Tweening.To(getter: () => renderer.color.a,
+                        setter: alpha => renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, alpha),
+                        to, duration).SetTarget(renderer);
+
+    }
+
+}
