76516d9be832ac0c0bad8ee32bb0abae227b6b29
Adam Graham <adam@zigurous.com>
2021-05-23 07:27:42 -0500

    Add new AudioDistortionFilter tweens

---
+++ Runtime/Extensions/AudioDistortionFilterTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class AudioDistortionFilterTweens
+    {
+        public static Tween TweenDistortionLevel(this AudioDistortionFilter filter, float to, float duration) =>
+            Tweener.To(getter: () => filter.distortionLevel,
+                       setter: distortionLevel => filter.distortionLevel = distortionLevel,
+                       to, duration).SetTarget(filter);
+
+    }
+
+}
