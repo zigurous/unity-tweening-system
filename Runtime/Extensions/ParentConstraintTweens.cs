4817d673dbd912f661fba05d4453b1590f83034f
Adam Graham <adam@zigurous.com>
2021-05-23 14:29:26 -0500

    Add new ParentConstraint tweens

---
+++ Runtime/Extensions/ParentConstraintTweens.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+
+namespace Zigurous.TweenEngine
+{
+    public static class ParentConstraintTweens
+    {
+        public static Tween TweenWeight(this ParentConstraint constraint, float to, float duration) =>
+            Tweening.To(getter: () => constraint.weight,
+                        setter: weight => constraint.weight = weight,
+                        to, duration).SetTarget(constraint);
+
+        public static Tween TweenRotationAtRest(this ParentConstraint constraint, Vector3 to, float duration) =>
+            Tweening.To(getter: () => constraint.rotationAtRest,
+                        setter: rotationAtRest => constraint.rotationAtRest = rotationAtRest,
+                        to, duration).SetTarget(constraint);
+
+        public static Tween TweenTranslationAtRest(this ParentConstraint constraint, Vector3 to, float duration) =>
+            Tweening.To(getter: () => constraint.translationAtRest,
+                        setter: translationAtRest => constraint.translationAtRest = translationAtRest,
+                        to, duration).SetTarget(constraint);
+
+    }
+
+}
