204cedc5e624856f2857ac193d69ec85ace593f6
Adam Graham <adam@zigurous.com>
2021-05-23 13:39:25 -0500

    Add new AspectRatioFitter tweens

---
+++ Runtime/Extensions/AspectRatioFitterTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+
+namespace Zigurous.TweenEngine
+{
+    public static class AspectRatioFitterTweens
+    {
+        public static Tween TweenAspectRatio(this AspectRatioFitter fitter, float to, float duration) =>
+            Tweening.To(getter: () => fitter.aspectRatio,
+                        setter: aspectRatio => fitter.aspectRatio = aspectRatio,
+                        to, duration).SetTarget(fitter);
+
+    }
+
+}
