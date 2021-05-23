fbfc6433212b644c4dc65c085fea4a1dfa25c76f
Adam Graham <adam@zigurous.com>
2021-05-23 14:07:02 -0500

    Add new WindZone tweens

---
+++ Runtime/Extensions/WindZoneTweens.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class WindZoneTweens
+    {
+        public static Tween TweenRadius(this WindZone windZone, float to, float duration) =>
+            Tweening.To(getter: () => windZone.radius,
+                        setter: radius => windZone.radius = radius,
+                        to, duration).SetTarget(windZone);
+
+        public static Tween TweenWindMain(this WindZone windZone, float to, float duration) =>
+            Tweening.To(getter: () => windZone.windMain,
+                        setter: windMain => windZone.windMain = windMain,
+                        to, duration).SetTarget(windZone);
+
+        public static Tween TweenWindTurbulence(this WindZone windZone, float to, float duration) =>
+            Tweening.To(getter: () => windZone.windTurbulence,
+                        setter: windTurbulence => windZone.windTurbulence = windTurbulence,
+                        to, duration).SetTarget(windZone);
+
+        public static Tween TweenWindPulseMagnitude(this WindZone windZone, float to, float duration) =>
+            Tweening.To(getter: () => windZone.windPulseMagnitude,
+                        setter: windPulseMagnitude => windZone.windPulseMagnitude = windPulseMagnitude,
+                        to, duration).SetTarget(windZone);
+
+        public static Tween TweenWindPulseFrequency(this WindZone windZone, float to, float duration) =>
+            Tweening.To(getter: () => windZone.windPulseFrequency,
+                        setter: windPulseFrequency => windZone.windPulseFrequency = windPulseFrequency,
+                        to, duration).SetTarget(windZone);
+
+    }
+
+}
