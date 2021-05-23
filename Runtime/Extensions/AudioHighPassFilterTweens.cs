9447cbac9dd740d746b41071a1e88960364d0a65
Adam Graham <adam@zigurous.com>
2021-05-23 07:28:00 -0500

    Add new AudioHighPassFilter tweens

---
+++ Runtime/Extensions/AudioHighPassFilterTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class AudioHighPassFilterTweens
+    {
+        public static Tween TweenCutoffFrequency(this AudioHighPassFilter filter, float to, float duration) =>
+            Tweener.To(getter: () => filter.cutoffFrequency,
+                       setter: cutoffFrequency => filter.cutoffFrequency = cutoffFrequency,
+                       to, duration).SetTarget(filter);
+
+        public static Tween TweenHighpassResonanceQ(this AudioHighPassFilter filter, float to, float duration) =>
+            Tweener.To(getter: () => filter.highpassResonanceQ,
+                       setter: highpassResonanceQ => filter.highpassResonanceQ = highpassResonanceQ,
+                       to, duration).SetTarget(filter);
+
+    }
+
+}
