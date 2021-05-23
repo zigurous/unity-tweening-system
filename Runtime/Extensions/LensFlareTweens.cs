92b6a6df08af4663bd0bb8cdf84e7d7ae594b8f5
Adam Graham <adam@zigurous.com>
2021-05-23 06:57:03 -0500

    Add new LensFlare tweens

---
+++ Runtime/Extensions/LensFlareTweens.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class LensFlareTweens
+    {
+        public static Tween TweenColor(this LensFlare lensFlare, Color to, float duration) =>
+            Tweener.To(getter: () => lensFlare.color,
+                       setter: color => lensFlare.color = color,
+                       to, duration).SetTarget(lensFlare);
+
+        public static Tween TweenBrightness(this LensFlare lensFlare, float to, float duration) =>
+            Tweener.To(getter: () => lensFlare.brightness,
+                       setter: brightness => lensFlare.brightness = brightness,
+                       to, duration).SetTarget(lensFlare);
+
+        public static Tween TweenFadeSpeed(this LensFlare lensFlare, float to, float duration) =>
+            Tweener.To(getter: () => lensFlare.fadeSpeed,
+                       setter: fadeSpeed => lensFlare.fadeSpeed = fadeSpeed,
+                       to, duration).SetTarget(lensFlare);
+
+    }
+
+}
