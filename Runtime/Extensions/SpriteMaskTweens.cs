c162df2a9ae8ff0e51f88ba9518ca6d9d1343147
Adam Graham <adam@zigurous.com>
2021-05-23 14:35:41 -0500

    Add new SpriteMask tweens

---
+++ Runtime/Extensions/SpriteMaskTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class SpriteMaskTweens
+    {
+        public static Tween TweenAlphaCutoff(this SpriteMask mask, float to, float duration) =>
+            Tweening.To(getter: () => mask.alphaCutoff,
+                        setter: alphaCutoff => mask.alphaCutoff = alphaCutoff,
+                        to, duration).SetTarget(mask);
+
+    }
+
+}
