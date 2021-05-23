0a572007db30a0c962f23e1b5d295ff31ffee532
Adam Graham <adam@zigurous.com>
2021-05-23 13:50:21 -0500

    Add new Canvas tweens

---
+++ Runtime/Extensions/CanvasTweens.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class CanvasTweens
+    {
+        public static Tween TweenScaleFactor(this Canvas canvas, float to, float duration) =>
+            Tweening.To(getter: () => canvas.scaleFactor,
+                        setter: scaleFactor => canvas.scaleFactor = scaleFactor,
+                        to, duration).SetTarget(canvas);
+
+        public static Tween TweenPlaneDistance(this Canvas canvas, float to, float duration) =>
+            Tweening.To(getter: () => canvas.planeDistance,
+                        setter: planeDistance => canvas.planeDistance = planeDistance,
+                        to, duration).SetTarget(canvas);
+
+        public static Tween TweenReferencePixelsPerUnit(this Canvas canvas, float to, float duration) =>
+            Tweening.To(getter: () => canvas.referencePixelsPerUnit,
+                        setter: referencePixelsPerUnit => canvas.referencePixelsPerUnit = referencePixelsPerUnit,
+                        to, duration).SetTarget(canvas);
+
+        public static Tween TweenNormalizedSortingGridSize(this Canvas canvas, float to, float duration) =>
+            Tweening.To(getter: () => canvas.normalizedSortingGridSize,
+                        setter: normalizedSortingGridSize => canvas.normalizedSortingGridSize = normalizedSortingGridSize,
+                        to, duration).SetTarget(canvas);
+
+    }
+
+}
