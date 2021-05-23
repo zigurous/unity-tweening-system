8dc8b627989d802a5e944f5534bb78bcc28b5b46
Adam Graham <adam@zigurous.com>
2021-05-23 16:38:37 -0500

    Add new TreePrototype tweens

---
+++ Runtime/Extensions/TreePrototypeTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class TreePrototypeTweens
+    {
+        public static Tween TweenTileSize(this TreePrototype prototype, float to, float duration) =>
+            Tweening.To(getter: () => prototype.bendFactor,
+                        setter: bendFactor => prototype.bendFactor = bendFactor,
+                        to, duration).SetTarget(prototype);
+
+    }
+
+}
