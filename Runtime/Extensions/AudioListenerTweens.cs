40eba92b3605be561a17b8c5d7a0f888e2664fd7
Adam Graham <adam@zigurous.com>
2021-05-23 07:28:19 -0500

    Add new AudioListener tweens

---
+++ Runtime/Extensions/AudioListenerTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Animation.Tweening
+{
+    public static class AudioListenerTweens
+    {
+        public static Tween TweenVolume(this AudioListener listener, float to, float duration) =>
+            Tweener.To(getter: () => AudioListener.volume,
+                       setter: volume => AudioListener.volume = volume,
+                       to, duration).SetTarget(listener);
+
+    }
+
+}
