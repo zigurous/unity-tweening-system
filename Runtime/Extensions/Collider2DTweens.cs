da178bdb6b7aedd1bdd7003c44b811641ff52536
Adam Graham <adam@zigurous.com>
2021-05-25 04:52:25 -0500

    Add new Collider2D tweens

---
+++ Runtime/Extensions/Collider2DTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class Collider2DTweens
+    {
+        public static Tween TweenDensity(this Collider2D collider, float to, float duration) =>
+            Tweening.To(getter: () => collider.density,
+                        setter: density => collider.density = density,
+                        to, duration).SetTarget(collider);
+
+        public static Tween TweenOffset(this Collider2D collider, Vector2 to, float duration) =>
+            Tweening.To(getter: () => collider.offset,
+                        setter: offset => collider.offset = offset,
+                        to, duration).SetTarget(collider);
+
+    }
+
+}
