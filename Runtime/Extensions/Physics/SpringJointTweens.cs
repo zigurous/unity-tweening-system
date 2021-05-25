d86719858b998d5c3a5d113a89b0b9fd4be24654
Adam Graham <adam@zigurous.com>
2021-05-25 07:33:47 -0500

    Add new SpringJoint tweens

---
+++ Runtime/Extensions/Physics/SpringJointTweens.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class SpringJointTweens
+    {
+        public static Tween TweenSpring(this SpringJoint joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.spring,
+                        setter: spring => joint.spring = spring,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenDamper(this SpringJoint joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.damper,
+                        setter: damper => joint.damper = damper,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenMinDistance(this SpringJoint joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.minDistance,
+                        setter: minDistance => joint.minDistance = minDistance,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenMaxDistance(this SpringJoint joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.maxDistance,
+                        setter: maxDistance => joint.maxDistance = maxDistance,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenTolerance(this SpringJoint joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.tolerance,
+                        setter: tolerance => joint.tolerance = tolerance,
+                        to, duration).SetTarget(joint);
+
+    }
+
+}
