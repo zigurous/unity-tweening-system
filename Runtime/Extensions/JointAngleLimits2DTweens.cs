e1f6589ea000faddb96a9a813cd509fce66e64fa
Adam Graham <adam@zigurous.com>
2021-05-25 05:34:54 -0500

    Add new JointAngleLimits2D tweens

---
+++ Runtime/Extensions/JointAngleLimits2DTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class JointAngleLimits2DTweens
+    {
+        public static Tween TweenMin(this JointAngleLimits2D angleLimits, float to, float duration) =>
+            Tweening.To(getter: () => angleLimits.min,
+                        setter: min => angleLimits.min = min,
+                        to, duration).SetTarget(angleLimits);
+
+        public static Tween TweenMax(this JointAngleLimits2D angleLimits, float to, float duration) =>
+            Tweening.To(getter: () => angleLimits.max,
+                        setter: max => angleLimits.max = max,
+                        to, duration).SetTarget(angleLimits);
+
+    }
+
+}
