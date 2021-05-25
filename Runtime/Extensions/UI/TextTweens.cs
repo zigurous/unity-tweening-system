149369dc1890dd2a27d72832aaeeed3d63dfa18a
Adam Graham <adam@zigurous.com>
2021-05-25 09:32:39 -0500

    Add new Text tweens

---
+++ Runtime/Extensions/UI/TextTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+
+namespace Zigurous.TweenEngine
+{
+    public static class TextTweens
+    {
+        public static Tween TweenFontSize(this Text text, int to, float duration) =>
+            Tweening.To(getter: () => text.fontSize,
+                        setter: fontSize => text.fontSize = fontSize,
+                        to, duration).SetTarget(text);
+
+    }
+
+}
