0ac59d4be2d8de9d073d400c1624570867234a3f
Adam Graham <adam@zigurous.com>
2021-05-25 05:33:41 -0500

    Add new AnchoredJoint2D tweens

---
+++ Runtime/Extensions/AnchoredJoint2DTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class AnchoredJoint2DTweens
+    {
+        public static Tween TweenAnchor(this AnchoredJoint2D joint, Vector2 to, float duration) =>
+            Tweening.To(getter: () => joint.anchor,
+                        setter: anchor => joint.anchor = anchor,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenConnectedAnchor(this AnchoredJoint2D joint, Vector2 to, float duration) =>
+            Tweening.To(getter: () => joint.connectedAnchor,
+                        setter: connectedAnchor => joint.connectedAnchor = connectedAnchor,
+                        to, duration).SetTarget(joint);
+
+    }
+
+}
