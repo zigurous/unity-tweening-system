6d54f981729dcf965f57bea2ad67ea6cb6ac0e3a
Adam Graham <adam@zigurous.com>
2021-05-25 09:13:46 -0500

    Add new Scrollbar tweens

---
+++ Runtime/Extensions/UI/ScrollbarTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UI;
+
+namespace Zigurous.TweenEngine
+{
+    public static class ScrollbarTweens
+    {
+        public static Tween TweenValue(this Scrollbar scrollbar, float to, float duration) =>
+            Tweening.To(getter: () => scrollbar.value,
+                        setter: value => scrollbar.value = value,
+                        to, duration).SetTarget(scrollbar);
+
+        public static Tween TweenSize(this Scrollbar scrollbar, float to, float duration) =>
+            Tweening.To(getter: () => scrollbar.size,
+                        setter: size => scrollbar.size = size,
+                        to, duration).SetTarget(scrollbar);
+
+    }
+
+}
