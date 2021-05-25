ce618a607dddb81a756063512dab7be429ac9710
Adam Graham <adam@zigurous.com>
2021-05-25 04:51:12 -0500

    Add new PhysicMaterial tweens

---
+++ Runtime/Extensions/PhysicMaterialTweens.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class PhysicMaterialTweens
+    {
+        public static Tween TweenBounciness(this PhysicMaterial material, float to, float duration) =>
+            Tweening.To(getter: () => material.bounciness,
+                        setter: bounciness => material.bounciness = bounciness,
+                        to, duration).SetTarget(material);
+
+        public static Tween TweenDynamicFriction(this PhysicMaterial material, float to, float duration) =>
+            Tweening.To(getter: () => material.dynamicFriction,
+                        setter: friction => material.dynamicFriction = friction,
+                        to, duration).SetTarget(material);
+
+        public static Tween TweenStaticFriction(this PhysicMaterial material, float to, float duration) =>
+            Tweening.To(getter: () => material.staticFriction,
+                        setter: friction => material.staticFriction = friction,
+                        to, duration).SetTarget(material);
+
+    }
+
+}
