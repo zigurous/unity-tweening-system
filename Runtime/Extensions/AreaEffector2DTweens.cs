2c4cc4e723a8ea2a6035d4641408ced1547d4fcc
Adam Graham <adam@zigurous.com>
2021-05-25 05:16:24 -0500

    Add new AreaEffector2D tweens

---
+++ Runtime/Extensions/AreaEffector2DTweens.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class AreaEffector2DTweens
+    {
+        public static Tween TweenForceAngle(this AreaEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.forceAngle,
+                        setter: forceAngle => effector.forceAngle = forceAngle,
+                        to, duration).SetTarget(effector);
+
+        public static Tween TweenForceMagnitude(this AreaEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.forceMagnitude,
+                        setter: forceMagnitude => effector.forceMagnitude = forceMagnitude,
+                        to, duration).SetTarget(effector);
+
+        public static Tween TweenForceVariation(this AreaEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.forceVariation,
+                        setter: forceVariation => effector.forceVariation = forceVariation,
+                        to, duration).SetTarget(effector);
+
+        public static Tween TweenDrag(this AreaEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.drag,
+                        setter: drag => effector.drag = drag,
+                        to, duration).SetTarget(effector);
+
+        public static Tween TweenAngularDrag(this AreaEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.angularDrag,
+                        setter: angularDrag => effector.angularDrag = angularDrag,
+                        to, duration).SetTarget(effector);
+
+    }
+
+}
