3815219eeca7f299d6ac767e96fc4cfd9f6a77bf
Adam Graham <adam@zigurous.com>
2021-05-25 05:55:07 -0500

    Add new SurfaceEffector2D tweens

---
+++ Runtime/Extensions/SurfaceEffector2DTweens.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class SurfaceEffector2DTweens
+    {
+        public static Tween TweenSpeed(this SurfaceEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.speed,
+                        setter: speed => effector.speed = speed,
+                        to, duration).SetTarget(effector);
+
+        public static Tween TweenSpeedVariation(this SurfaceEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.speedVariation,
+                        setter: speedVariation => effector.speedVariation = speedVariation,
+                        to, duration).SetTarget(effector);
+
+        public static Tween TweenForceScale(this SurfaceEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.forceScale,
+                        setter: forceScale => effector.forceScale = forceScale,
+                        to, duration).SetTarget(effector);
+
+    }
+
+}
