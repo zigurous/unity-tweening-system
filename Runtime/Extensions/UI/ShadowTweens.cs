273f03753cbcbbc73d92008da7a26ea3e6f097ad
Adam Graham <adam@zigurous.com>
2021-05-25 09:13:14 -0500

    Add new Shadow tweens

---
+++ Runtime/Extensions/UI/ShadowTweens.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zigurous.TweenEngine
+{
+    public static class ShadowTweens
+    {
+        public static Tween TweenEffectColor(this Shadow shadow, Color to, float duration) =>
+            Tweening.To(getter: () => shadow.effectColor,
+                        setter: effectColor => shadow.effectColor = effectColor,
+                        to, duration).SetTarget(shadow);
+
+        public static Tween TweenEffectDistance(this Shadow shadow, Vector2 to, float duration) =>
+            Tweening.To(getter: () => shadow.effectDistance,
+                        setter: effectDistance => shadow.effectDistance = effectDistance,
+                        to, duration).SetTarget(shadow);
+
+    }
+
+}
