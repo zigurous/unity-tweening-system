5343f8181a481d7b3f23de8f2d54e06738865410
Adam Graham <adam@zigurous.com>
2021-05-25 05:47:05 -0500

    Add new SliderJoint2D tweens

---
+++ Runtime/Extensions/SliderJoint2DTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class SliderJoint2DTweens
+    {
+        public static Tween TweenAngle(this SliderJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.angle,
+                        setter: angle => joint.angle = angle,
+                        to, duration).SetTarget(joint);
+
+    }
+
+}
