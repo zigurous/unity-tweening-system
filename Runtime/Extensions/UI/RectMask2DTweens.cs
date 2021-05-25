be5098e430dbed66f037e262dba356a0105676ed
Adam Graham <adam@zigurous.com>
2021-05-25 09:13:29 -0500

    Add new RectMask2D tweens

---
+++ Runtime/Extensions/UI/RectMask2DTweens.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zigurous.TweenEngine
+{
+    public static class RectMask2DTweens
+    {
+        public static Tween TweenPadding(this RectMask2D mask, Vector4 to, float duration) =>
+            Tweening.To(getter: () => mask.padding,
+                        setter: padding => mask.padding = padding,
+                        to, duration).SetTarget(mask);
+
+        public static Tween TweenSoftness(this RectMask2D mask, Vector2Int to, float duration) =>
+            Tweening.To(getter: () => mask.softness,
+                        setter: softness => mask.softness = softness,
+                        to, duration).SetTarget(mask);
+
+    }
+
+}
