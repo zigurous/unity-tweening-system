d2c91961de69e6fe7a7d876a7b6e8eef8cdd5099
Adam Graham <adam@zigurous.com>
2021-05-25 05:49:10 -0500

    Add new JointTranslationLimits2D tweens

--- Runtime/Extensions/JointTranslationLimits2DTweens.cs
+++ Runtime/Extensions/JointTranslationLimits2DTweens.cs
@@ -2,14 +2,14 @@
 
 namespace Zigurous.TweenEngine
 {
-    public static class JointAngleLimits2DTweens
+    public static class JointTranslationLimits2DTweens
     {
-        public static Tween TweenMin(this JointAngleLimits2D limits, float to, float duration) =>
+        public static Tween TweenMin(this JointTranslationLimits2D limits, float to, float duration) =>
             Tweening.To(getter: () => limits.min,
                         setter: min => limits.min = min,
                         to, duration).SetTarget(limits);
 
-        public static Tween TweenMax(this JointAngleLimits2D limits, float to, float duration) =>
+        public static Tween TweenMax(this JointTranslationLimits2D limits, float to, float duration) =>
             Tweening.To(getter: () => limits.max,
                         setter: max => limits.max = max,
                         to, duration).SetTarget(limits);
