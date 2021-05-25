1c2b665b48ed1c90e84004479c387378f24d7292
Adam Graham <adam@zigurous.com>
2021-05-25 05:51:50 -0500

    Add new SpringJoint2D tweens

---
+++ Runtime/Extensions/SpringJoint2DTweens.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class SpringJoint2DTweens
+    {
+        public static Tween TweenDistance(this SpringJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.distance,
+                        setter: distance => joint.distance = distance,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenDampingRatio(this SpringJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.dampingRatio,
+                        setter: dampingRatio => joint.dampingRatio = dampingRatio,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenFrequency(this SpringJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.frequency,
+                        setter: frequency => joint.frequency = frequency,
+                        to, duration).SetTarget(joint);
+
+    }
+
+}
