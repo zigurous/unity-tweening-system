68d0aa937735cddda98d2bbf4c51ae6a7ab887c6
Adam Graham <adam@zigurous.com>
2021-05-25 04:58:00 -0500

    Add new EdgeCollider2D tweens

---
+++ Runtime/Extensions/EdgeCollider2DTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class EdgeCollider2DTweens
+    {
+        public static Tween TweenEdgeRadius(this EdgeCollider2D collider, float to, float duration) =>
+            Tweening.To(getter: () => collider.edgeRadius,
+                        setter: edgeRadius => collider.edgeRadius = edgeRadius,
+                        to, duration).SetTarget(collider);
+
+    }
+
+}
