4ca3d7e2b67b5a53d181aad374615debc40c4b65
Adam Graham <adam@zigurous.com>
2021-05-25 05:12:48 -0500

    Add new CircleCollider2D tweens

---
+++ Runtime/Extensions/CircleCollider2DTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class CircleCollider2DTweens
+    {
+        public static Tween TweenRadius(this CircleCollider2D collider, float to, float duration) =>
+            Tweening.To(getter: () => collider.radius,
+                        setter: radius => collider.radius = radius,
+                        to, duration).SetTarget(collider);
+
+    }
+
+}
