b9c55b5cfa1bbe1e9207ea657e1bf40b678792e2
Adam Graham <adam@zigurous.com>
2021-05-25 05:39:42 -0500

    Add new PlatformEffector2D tweens

---
+++ Runtime/Extensions/PlatformEffector2DTweens.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class PlatformEffector2DTweens
+    {
+        public static Tween TweenSurfaceArc(this PlatformEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.surfaceArc,
+                        setter: surfaceArc => effector.surfaceArc = surfaceArc,
+                        to, duration).SetTarget(effector);
+
+        public static Tween TweenSideArc(this PlatformEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.sideArc,
+                        setter: sideArc => effector.sideArc = sideArc,
+                        to, duration).SetTarget(effector);
+
+        public static Tween TweenRotationalOffset(this PlatformEffector2D effector, float to, float duration) =>
+            Tweening.To(getter: () => effector.rotationalOffset,
+                        setter: rotationalOffset => effector.rotationalOffset = rotationalOffset,
+                        to, duration).SetTarget(effector);
+
+    }
+
+}
