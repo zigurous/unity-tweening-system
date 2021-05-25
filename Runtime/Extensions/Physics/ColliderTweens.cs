f9003152ecfc7ed050dde976364e18bf00e0d39e
Adam Graham <adam@zigurous.com>
2021-05-25 07:32:33 -0500

    Add new Collider tweens

---
+++ Runtime/Extensions/Physics/ColliderTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class ColliderTweens
+    {
+        public static Tween TweenContactOffset(this Collider collider, float to, float duration) =>
+            Tweening.To(getter: () => collider.contactOffset,
+                        setter: contactOffset => collider.contactOffset = contactOffset,
+                        to, duration).SetTarget(collider);
+
+    }
+
+}
