b057425be7f663f4dff766bcf255246a18922290
Adam Graham <adam@zigurous.com>
2021-05-25 05:34:36 -0500

    Add new FrictionJoint2D tweens

---
+++ Runtime/Extensions/FrictionJoint2DTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class FrictionJoint2DTweens
+    {
+        public static Tween TweenMaxForce(this FrictionJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.maxForce,
+                        setter: maxForce => joint.maxForce = maxForce,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenMaxTorque(this FrictionJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.maxTorque,
+                        setter: maxTorque => joint.maxTorque = maxTorque,
+                        to, duration).SetTarget(joint);
+
+    }
+
+}
