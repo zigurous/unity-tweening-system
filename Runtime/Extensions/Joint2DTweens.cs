5cf682c020f9a5afee60ef74a533151c18dc945e
Adam Graham <adam@zigurous.com>
2021-05-25 05:34:44 -0500

    Add new Joint2D tweens

---
+++ Runtime/Extensions/Joint2DTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class Joint2DTweens
+    {
+        public static Tween TweenBreakForce(this Joint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.breakForce,
+                        setter: breakForce => joint.breakForce = breakForce,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenBreakTorque(this Joint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.breakTorque,
+                        setter: breakTorque => joint.breakTorque = breakTorque,
+                        to, duration).SetTarget(joint);
+
+    }
+
+}
