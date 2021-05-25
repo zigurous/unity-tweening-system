fdb17deeb78a9029a2355de757338dd5125b36f5
Adam Graham <adam@zigurous.com>
2021-05-25 05:34:21 -0500

    Add new FixedJoint2D tweens

---
+++ Runtime/Extensions/FixedJoint2DTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class FixedJoint2DTweens
+    {
+        public static Tween TweenDampingRatio(this FixedJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.dampingRatio,
+                        setter: dampingRatio => joint.dampingRatio = dampingRatio,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenFrequency(this FixedJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.frequency,
+                        setter: frequency => joint.frequency = frequency,
+                        to, duration).SetTarget(joint);
+
+    }
+
+}
