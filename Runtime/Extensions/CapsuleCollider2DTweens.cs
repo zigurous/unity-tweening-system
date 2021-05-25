3964124fe1e2e244a74a5e48d98ea8ee1fe5d3ed
Adam Graham <adam@zigurous.com>
2021-05-25 04:53:18 -0500

    Add new CapsuleCollider2D tweens

---
+++ Runtime/Extensions/CapsuleCollider2DTweens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class CapsuleCollider2DTweens
+    {
+        public static Tween TweenSize(this CapsuleCollider2D collider, Vector2 to, float duration) =>
+            Tweening.To(getter: () => collider.size,
+                        setter: size => collider.size = size,
+                        to, duration).SetTarget(collider);
+
+    }
+
+}
