e4a99fd2584bad957e358f9f78d471e201ca56c5
Adam Graham <adam@zigurous.com>
2021-05-25 06:01:24 -0500

    Add new JointSuspension2D tweens

---
+++ Runtime/Extensions/JointSuspension2DTweens.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class JointSuspension2DTweens
+    {
+        public static Tween TweenDampingRatio(this JointSuspension2D suspension, float to, float duration) =>
+            Tweening.To(getter: () => suspension.dampingRatio,
+                        setter: dampingRatio => suspension.dampingRatio = dampingRatio,
+                        to, duration).SetTarget(suspension);
+
+        public static Tween TweenFrequency(this JointSuspension2D suspension, float to, float duration) =>
+            Tweening.To(getter: () => suspension.frequency,
+                        setter: frequency => suspension.frequency = frequency,
+                        to, duration).SetTarget(suspension);
+
+        public static Tween TweenAngle(this JointSuspension2D suspension, float to, float duration) =>
+            Tweening.To(getter: () => suspension.angle,
+                        setter: angle => suspension.angle = angle,
+                        to, duration).SetTarget(suspension);
+
+    }
+
+}
