390f34fe892bdb9587a8d7c9760c392c0e278e4f
Adam Graham <adam@zigurous.com>
2021-05-25 05:45:04 -0500

    Add new RelativeJoint2D tweens

---
+++ Runtime/Extensions/RelativeJoint2DTweens.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class RelativeJoint2DTweens
+    {
+        public static Tween TweenMaxForce(this RelativeJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.maxForce,
+                        setter: maxForce => joint.maxForce = maxForce,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenMaxTorque(this RelativeJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.maxTorque,
+                        setter: maxTorque => joint.maxTorque = maxTorque,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenCorrectionScale(this RelativeJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.correctionScale,
+                        setter: correctionScale => joint.correctionScale = correctionScale,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenLinearOffset(this RelativeJoint2D joint, Vector2 to, float duration) =>
+            Tweening.To(getter: () => joint.linearOffset,
+                        setter: linearOffset => joint.linearOffset = linearOffset,
+                        to, duration).SetTarget(joint);
+
+        public static Tween TweenAngularOffset(this RelativeJoint2D joint, float to, float duration) =>
+            Tweening.To(getter: () => joint.angularOffset,
+                        setter: angularOffset => joint.angularOffset = angularOffset,
+                        to, duration).SetTarget(joint);
+
+    }
+
+}
