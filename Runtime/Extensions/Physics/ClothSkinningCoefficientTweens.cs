af9daace8cff14a51443dce056ee9fd63bcea83f
Adam Graham <adam@zigurous.com>
2021-05-25 07:31:21 -0500

    Add new ClothSkinningCoefficient tweens

---
+++ Runtime/Extensions/Physics/ClothSkinningCoefficientTweens.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Zigurous.TweenEngine
+{
+    public static class ClothSkinningCoefficientTweens
+    {
+        public static Tween TweenMaxDistance(this ClothSkinningCoefficient coefficient, float to, float duration) =>
+            Tweening.To(getter: () => coefficient.maxDistance,
+                        setter: maxDistance => coefficient.maxDistance = maxDistance,
+                        to, duration).SetTarget(coefficient);
+
+        public static Tween TweenCollisionSphereDistance(this ClothSkinningCoefficient coefficient, float to, float duration) =>
+            Tweening.To(getter: () => coefficient.collisionSphereDistance,
+                        setter: collisionSphereDistance => coefficient.collisionSphereDistance = collisionSphereDistance,
+                        to, duration).SetTarget(coefficient);
+
+    }
+
+}
