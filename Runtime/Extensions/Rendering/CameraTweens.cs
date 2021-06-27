5c19c9a6438a6a1503def70e69f1bcd3b494adba
Adam Graham <adam@zigurous.com>
2021-06-26 22:32:54 -0500

    Add new camera tweens

--- Runtime/Extensions/Rendering/CameraTweens.cs
+++ Runtime/Extensions/Rendering/CameraTweens.cs
@@ -39,6 +39,36 @@ namespace Zigurous.Tweening
                         setter: sensorSize => camera.sensorSize = sensorSize,
                         to, duration).SetTarget(camera);
 
+        public static Tween TweenFocalLength(this Camera camera, float to, float duration) =>
+            Tweening.To(getter: () => camera.focalLength,
+                        setter: focalLength => camera.focalLength = focalLength,
+                        to, duration).SetTarget(camera);
+
+        public static Tween TweenStereoConvergence(this Camera camera, float to, float duration) =>
+            Tweening.To(getter: () => camera.stereoConvergence,
+                        setter: stereoConvergence => camera.stereoConvergence = stereoConvergence,
+                        to, duration).SetTarget(camera);
+
+        public static Tween TweenStereoSeparation(this Camera camera, float to, float duration) =>
+            Tweening.To(getter: () => camera.stereoSeparation,
+                        setter: stereoSeparation => camera.stereoSeparation = stereoSeparation,
+                        to, duration).SetTarget(camera);
+
+        public static Tween TweenLensShift(this Camera camera, Vector2 to, float duration) =>
+            Tweening.To(getter: () => camera.lensShift,
+                        setter: lensShift => camera.lensShift = lensShift,
+                        to, duration).SetTarget(camera);
+
+        public static Tween TweenRect(this Camera camera, Rect to, float duration) =>
+            Tweening.To(getter: () => camera.rect,
+                        setter: rect => camera.rect = rect,
+                        to, duration).SetTarget(camera);
+
+        public static Tween TweenPixelRect(this Camera camera, Rect to, float duration) =>
+            Tweening.To(getter: () => camera.pixelRect,
+                        setter: pixelRect => camera.pixelRect = pixelRect,
+                        to, duration).SetTarget(camera);
+
         public static Tween TweenDepth(this Camera camera, float to, float duration) =>
             Tweening.To(getter: () => camera.depth,
                         setter: depth => camera.depth = depth,
